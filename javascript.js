c6816e0cbaefefb56a54db1065ed65c21093a6de
Wyn <206344000+WynIvy@users.noreply.github.com>
2025-05-29 09:39:51 -0400

    added javascript messages function

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const messages = [
+    { id: 1, quote: "The Sun is Evil"},
+    {id: 2, quote: "The Moon is watching."},
+    {id: 3, quote: "Mercury is fucking with you again."},
+    {id: 4, quote: "Saturn has the last laugh."},
+    {id: 5, wuote: "Jupiter is .... trying"}
+    ]
+
+const button = document.getElementById('button')
+
+button.addEventListener("click", function() {
+    newQuote()
+})
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (messages.length));
+    document.getElementById("message").innerHTML = messages[randomNumber].quote;
+}
