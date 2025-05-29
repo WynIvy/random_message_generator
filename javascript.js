2f465d0ec428829310e9b81a512df276ada7a520
Wyn <206344000+WynIvy@users.noreply.github.com>
2025-05-29 09:52:14 -0400

    fixed javascript code to run after dom is loaded

--- javascript.js
+++ javascript.js
@@ -1,18 +1,19 @@
 const messages = [
-    { id: 1, quote: "The Sun is Evil"},
+    {id: 1, quote: "The Sun is Evil"},
     {id: 2, quote: "The Moon is watching."},
     {id: 3, quote: "Mercury is fucking with you again."},
     {id: 4, quote: "Saturn has the last laugh."},
-    {id: 5, wuote: "Jupiter is .... trying"}
+    {id: 5, quote: "Jupiter is .... trying"}
     ]
 
-const button = document.getElementById('button')
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('button');
+    button.addEventListener("click", function() {
+        newQuote();
+    });
 
-button.addEventListener("click", function() {
-    newQuote()
-})
-
-function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (messages.length));
-    document.getElementById("message").innerHTML = messages[randomNumber].quote;
-}
+    function newQuote() {
+        var randomNumber = Math.floor(Math.random() * (messages.length));
+        document.getElementById("message").innerHTML = messages[randomNumber].quote;
+    }
+});
