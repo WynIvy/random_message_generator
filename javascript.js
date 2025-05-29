c3d6ff020dd163284bb8b8cc4d8d4a0767732990
Wyn <206344000+WynIvy@users.noreply.github.com>
2025-05-29 11:12:15 -0400

    fixed some issues with the divs. added some more quotes and changed a few

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 const messages = [
-    {id: 1, quote: "The Sun is Evil"},
-    {id: 2, quote: "The Moon is watching."},
-    {id: 3, quote: "Mercury is fucking with you again."},
-    {id: 4, quote: "Saturn has the last laugh."},
-    {id: 5, quote: "Jupiter is .... trying"},
+    {id: 1, quote: "The Sun is evil"},
+    {id: 2, quote: "The Moon full of delights"},
+    {id: 3, quote: "Mercury is fucking with you<br>...again..."},
+    {id: 4, quote: "Saturn has the last laugh.<br>HAHAHA"},
+    {id: 5, quote: "Jupiter is .... trying<br>...so hard..."},
     {id: 6, quote: "Pisces is sexy"},
-    {id: 7, quote: "Venus is delicious"},
-    {id: 8, quote: "Jupiter wants to hug you"}
+    {id: 7, quote: "Venus is pretty neat"},
+    {id: 8, quote: "Mars hurts :("},
+    {id: 9, quote: "The Moon is empty"},
+    {id: 10, quote: "The Sun is touching you with its rays.<br>(oww)"},
+    {id: 11, quote: "Saturn yearns for you to be stuck in traffic."}
     ]
 
 document.addEventListener('DOMContentLoaded', function() {
